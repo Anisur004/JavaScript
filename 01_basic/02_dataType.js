ef700004025c57c971de47fe79e239a8eca4fdcd
Anisur004 <167961544+Anisur004@users.noreply.github.com>
2024-10-19 12:55:25 +0000

    dataType

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict";//treat all JS CODE AS NEWER VERSION
+//alert(3+3) we are using node js,notbrowser
+let name="Anisur"
+let age=18
+let isLogged=false
+/*
+numbrer=>2 to power 53
+bigint
+string=>""
+boolean=>true/false
+null=>standalone value
+undefined=>
+symbol=>uniqeness
+object
+
+*/
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof name)
