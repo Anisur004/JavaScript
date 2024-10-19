5ebf99e4207befaf6adfdba8c7201091e315e4b2
Anisur004 <167961544+Anisur004@users.noreply.github.com>
2024-10-19 12:40:16 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=123
+let accountEmail="anisur@.com"
+var accountPassword="123"
+accountCity ="jaipur"
+let accountState;
+ //accountId=2 not allowd
+ console.log(accountId);
+ accountEmail="rahman@123";
+ accountPassword="123435"
+ accountCity="gaya"
+ console. table([accountId,accountEmail,accountPassword,accountCity])
+ /*
+ prefer not to use var
+beacuse  of issue in block scope and functional scope*/
