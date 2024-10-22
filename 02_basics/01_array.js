573560c0496c2edbca71bd1d85573a87d91add72
Anisur004 <167961544+Anisur004@users.noreply.github.com>
2024-10-22 11:08:33 +0000

    Array1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,31 @@
+const myArray=[0,1,2,3,4,]
+const myArray2= new Array(0,2,5,)
+//console.log(myArray[3]);
+
+
+//Array method
+// myArray.push(6)
+// myArray.push(7)
+// console.log(myArray);
+// myArray.pop(8)
+
+// 
+
+// const newarr= myArray.join()
+// console.log(myArray);
+// console.log(newarr);// now it is in string type
+
+
+//slice,splice
+console.log("A",myArray);
+
+const myn1=myArray.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArray);
+const myn2=myArray.splice(1,3)
+console.log("C",myArray);
+console.log(myn2);
+
+
+
