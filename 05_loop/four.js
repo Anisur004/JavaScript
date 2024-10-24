df3f5c00ed406dfa1a566fd0351f6f9ba5672034
Anisur004 <167961544+Anisur004@users.noreply.github.com>
2024-10-24 08:28:10 +0000

    ForIn_loop

---
+++ 05_loop/four.js
@@ -0,0 +1,28 @@
+//forIn loop
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+//forIn loop using array
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+//forIn loop in object not iterable
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
