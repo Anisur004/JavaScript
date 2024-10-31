00984a40946813f0f65a23a84c34bd09070565ea
Anisur004 <167961544+Anisur004@users.noreply.github.com>
2024-10-31 04:22:38 +0000

    inheritance&static

--- 10_oops/inheritance.js
+++ 10_oops/inheritance.js
@@ -28,3 +28,34 @@ const masalaChai = new User("masalaChai")
 masalaChai.logMe()
 
 console.log(chai instanceof User);
+
+
+
+
+//static
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
