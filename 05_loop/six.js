fe38c76bf36da696d93ee098517b348c9dafe57a
Anisur004 <167961544+Anisur004@users.noreply.github.com>
2024-12-29 23:11:40 +0530

    Update six.js

--- 05_loop/six.js
+++ 05_loop/six.js
@@ -29,8 +29,13 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // const newNums = myNums.filter( (num) => {
 //     return num > 4
 // } )
+//Use forEach
+const nwNum=[]
 
-
+myNum.forEach((num)=>{
+    if(num>4){
+        newNum.push(num)}
+})
 
 
 
