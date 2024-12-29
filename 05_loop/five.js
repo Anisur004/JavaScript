90648411e9b7d8e81d99a680385c3e5a32bc1d3a
Anisur004 <167961544+Anisur004@users.noreply.github.com>
2024-12-29 22:56:05 +0530

    Update five.js

--- 05_loop/five.js
+++ 05_loop/five.js
@@ -4,7 +4,7 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 //     console.log(val);
 // } )
 //another ways
-coding.forEach( item => {
+coding.forEach( (item) => {
     //console.log(item);
 } )
 
