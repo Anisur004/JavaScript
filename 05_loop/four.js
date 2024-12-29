c4664da7cb507300d96fa0106aa542d890b7ebe0
Anisur004 <167961544+Anisur004@users.noreply.github.com>
2024-12-29 22:46:04 +0530

    Update four.js

--- 05_loop/four.js
+++ 05_loop/four.js
@@ -16,7 +16,7 @@ const programming = ["js", "rb", "py", "java", "cpp"]
 for (const key in programming) {
     console.log(programming[key]);
 }
-//forIn loop in object not iterable
+//forIn loop in map not iterable
 // const map = new Map()
 // map.set('IN', "India")
 // map.set('USA', "United States of America")
